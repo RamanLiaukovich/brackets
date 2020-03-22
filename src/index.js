d4192c1186178e9ade886dc09d28724cb74b50b6
Raman Liakovich <2038634@gmail.com>
2020-03-22 22:25:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let index = bracketsConfig.flat().indexOf(str[i]);
+    let index1 = bracketsConfig.flat().indexOf(str[i], index + 1);
+    if (index !== -1 && index1 === -1) {
+      if (index % 2 === 0) {
+        stack.push(str[i]);
+      } else if (bracketsConfig.flat().indexOf(stack[stack.length - 1]) === index - 1) {
+        stack.pop();
+      }
+    if (index1 !== -1) {
+      if (index % 2 === 0) {
+        stack.push(str[i]);
+      } else if (bracketsConfig.flat().indexOf(stack[stack.length - 1]) === index1 - 1 && (index1 % 2 !== 0)) {
+        stack.pop();
+      }
+    }
+   
+      // else 
+      // } else if (index1 !== -1 && bracketsConfig.flat().indexOf(stack[stack.length - 1]) === (index1 - 1)) {
+      //   stack.pop();
+      // } 
+      // if (stack.pop() !== undefined) {
+      //   return true;
+      // } else return false;
+    // }
+    // if 
+    // return false;
+  } 
+  }
+  if (stack.length !== 0) return false;
+    else return true;
 }
